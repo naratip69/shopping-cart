96a80ef3ad07da2971f1f20ad4751e4d17c616b4
Naratip <naratip.18257@gmail.com>
2023-07-16 18:44:43 +0700

    test ItemComponent

--- src/component/Item.test.js
+++ src/component/Item.test.js
@@ -10,12 +10,30 @@ test("props", () => {
 });
 
 test("button", async () => {
-  // const user = userEvent.setup();
+  const user = userEvent.setup();
   render(<Item name="item1" price={10} />);
 
   const plusButt = screen.getByRole("button", { name: "+" });
   const minusButt = screen.getByRole("button", { name: "-" });
 
   const text = screen.getByRole("spinbutton");
+  expect(+text.value).toBe(0);
+
+  await user.click(plusButt);
+  expect(+text.value).toBe(1);
+  await user.click(minusButt);
+  expect(+text.value).toBe(0);
+  await user.click(minusButt);
+  expect(+text.value).toBe(0);
+});
+
+test("text", async () => {
+  const user = userEvent.setup();
+  render(<Item name="item1" price={10} />);
+
+  const text = screen.getByRole("spinbutton");
+  await user.type(text, "10");
+  expect(+text.value).toBe(10);
+  await user.type(text, "-");
   expect(+text.value).toBe(0);
 });
