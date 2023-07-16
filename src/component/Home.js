246714a1d32fad373948900779a275d5edc772ee
Naratip <naratip.18257@gmail.com>
2023-07-16 20:50:16 +0700

    add function add/remove itemList

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 
 export default function Home() {
+  const [itemList, setItemList] = useState([]);
+
+  function addItemList(obj) {
+    setItemList((list) => [...list, obj]);
+  }
+
+  function removeItemList(name) {
+    setItemList((list) => list.filter((e) => e.name !== name));
+  }
+
   return (
     <div className="home">
       <h1>Home</h1>
