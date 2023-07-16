94eaa1ec135a7cf027d9626c0b720fe04f95d132
Naratip <naratip.18257@gmail.com>
2023-07-16 21:05:59 +0700

    add Item to DOM

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
+import Item from "./Item";
 
 export default function Home() {
   const [itemList, setItemList] = useState([]);
@@ -12,10 +13,35 @@ export default function Home() {
     setItemList((list) => list.filter((e) => e.name !== name));
   }
 
+  const items = [
+    <Item
+      name="Candy"
+      price={10}
+      addItemList={addItemList}
+      removeItemList={removeItemList}
+    />,
+    <Item
+      name="Chips"
+      price={28}
+      addItemList={addItemList}
+      removeItemList={removeItemList}
+    />,
+    <Item
+      name="Cola"
+      price={15}
+      addItemList={addItemList}
+      removeItemList={removeItemList}
+    />,
+  ];
+
+  useEffect(() => {
+    console.log(itemList);
+  }, [itemList]);
   return (
     <div className="home">
       <h1>Home</h1>
       <Navbar />
+      <div className="items">{items.map((e) => e)}</div>
     </div>
   );
 }
--- src/component/Item.js
+++ src/component/Item.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function Item({ name, price }) {
+export default function Item({ name, price, addItemList, removeItemList }) {
   const [amount, setAmount] = useState(0);
   const [isInCart, setInCart] = useState(false);
 
@@ -14,6 +14,22 @@ export default function Item({ name, price }) {
     setAmount((current) => {
       return num >= 0 ? num : 0;
     });
+  }
+
+  function addToCartHandle(e) {
+    if (amount === 0) return;
+    if (isInCart) {
+      removeItemList(name);
+      setInCart((current) => !current);
+    } else {
+      const obj = {
+        name: name,
+        price: price,
+        amount: amount,
+      };
+      addItemList(obj);
+      setInCart((current) => !current);
+    }
   }
 
   return (
@@ -37,7 +53,7 @@ export default function Item({ name, price }) {
         <button type="button" onClick={(e) => addAmount(1)} disabled={isInCart}>
           +
         </button>
-        <button type="button" onClick={(e) => setInCart((current) => !current)}>
+        <button type="button" onClick={addToCartHandle}>
           {!isInCart ? "Add to cart" : "Edit Amount"}
         </button>
       </div>
