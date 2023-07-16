beb084cdaa3d3f710504421744be705817defa36
Naratip <naratip.18257@gmail.com>
2023-07-16 17:47:17 +0700

    add Navbar component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
 
 export default function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
+      <Navbar />
     </div>
   );
 }
--- src/component/ShoppingCart.js
+++ src/component/ShoppingCart.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
 
 export default function ShoppingCart() {
   return (
     <div className="shop">
       <h1>ShoppingCart</h1>
+      <Navbar />
     </div>
   );
 }
