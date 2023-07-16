0441f34cf9025a138dc4f686d5db25152880cdaf
Naratip <naratip.18257@gmail.com>
2023-07-16 17:29:34 +0700

    add Home and ShoppingCart header

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,1 +1,9 @@
+import React, { useState, useEffect } from "react";
 
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
--- src/component/ShoppingCart.js
+++ src/component/ShoppingCart.js
@@ -1,1 +1,9 @@
+import React, { useState, useEffect } from "react";
 
+export default function ShoppingCart() {
+  return (
+    <div className="shop">
+      <h1>ShoppingCart</h1>
+    </div>
+  );
+}
